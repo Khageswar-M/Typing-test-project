d7a7d6c4aa881cb3b108c225642b3284bf7b9ec9
Khageswar-M <ok3programmer@gmail.com>
2025-07-22 02:23:45 +0530

    On 22-07-2025

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
 let originalText = "";
 let charSpans = [];
 const wordLengthInput = document.getElementById("wordLength");
-const words = document.getElementById("words");
-const countCorrect = document.getElementById("correctCount");
-const countIncorrect = document.getElementById("incorrectCount");
 const inputField = document.getElementById("test");
+const wordsCount = document.querySelector(".wordsC");
+
+
 
 document.addEventListener("keydown", (event)=>{
-    inputField.focus();
+    if(event.key === 'Enter'){
+      inputField.focus();
+      startTimmer();
+    };
+    if(event.key === 'Backspace' || event.key === 'Delete' || event.key.startsWith("Arrow")){
+      event.preventDefault();
+    }
 });
-
+ 
 let correctCount = 0;
 let incorrectCount = 0;
+let countWords = 0;
 
-words.style.fontSize = "2rem";
-words.style.fontFamily = "monospace";
+let allWords = 0;
+let totalLength = 0;
 
+
+// words.classList.add("word");
+
+getWords();
 async function getWords() {
   try {
     const len = wordLengthInput.value || 10;
     const apiUrl = `https://random-word-api.vercel.app/api?words=${len}`;
     const getData = await fetch(apiUrl);
     const datas = await getData.json();
-
-    document.getElementById(
-      "currLength"
-    ).textContent = `Current length: ${datas.length}`;
+    allWords = datas.length;
     originalText = datas.join(" ") + " ";
-    console.log(originalText);
     renderCharacters(originalText.split(""));
     inputField.value = "";
 
@@ -40,21 +47,22 @@ async function getWords() {
     console.error("Fetch failed:", error);
   }
 }
-
 function renderCharacters(chars) {
-  const fragment = document.createDocumentFragment();
+  totalLength = chars.length;
+  const textSection = document.querySelector(".text-section");
   charSpans = [];
 
+  textSection.innerHTML = "";
   chars.forEach((ch) => {
     const span = document.createElement("span");
     span.textContent = ch;
-    fragment.appendChild(span);
+    textSection.appendChild(span);
     charSpans.push(span);
   });
 
-  words.innerHTML = "";
-  words.appendChild(fragment);
 }
+
+let stack = [];
 
 function check(inputField) {
   const currLength = inputField.value.length;
@@ -68,14 +76,24 @@ function check(inputField) {
     const span = charSpans[index];
 
     if (inputChar === originalChar) {
+      // if(inputChar === " " || index === totalLength){
+      //   stack.push(incorrectCount);
+      //   if(stack.length === 1 && stack[stack.length - 1] === 0){
+      //     countWords++;
+      //   }else if(stack.length > 1 && stack[stack.length - 1] === stack[stack.length - 2]){
+      //     countWords++;
+      //   }
+      // }
       correctCount++;
       correctnessArray[index] = true;
       if (span) {
         span.classList.add("correct");
         span.classList.remove("incorrect");
       }
+      checkWords(inputChar, index);
     } else {
       incorrectCount++;
+      // stack.push(incorrectCount);
       correctnessArray[index] = false;
       if (span) {
         span.classList.add("incorrect");
@@ -99,9 +117,10 @@ function check(inputField) {
     correctnessArray.splice(index, 1);
     prevLength = currLength;
   }
-  countCorrect.textContent = `Correct: ${correctCount}`;
-  countIncorrect.textContent = `In-correct: ${incorrectCount}`;
+
+  wordsCount.textContent = `${countWords}/${allWords}`;
 }
+
 inputField.addEventListener("input", () => {
   // Prevent multiple consecutive spaces
   inputField.value = inputField.value.replace(/ {2,}/g, " ");
@@ -114,10 +133,73 @@ inputField.addEventListener("input", () => {
     );
   }
 
+
   const totalTyped = correctCount + incorrectCount;
-  if (totalTyped < originalText.length) {
+  if (totalTyped < totalLength - 2) {
     check(inputField);
-  } else {
-    console.log("Typing test finished.");
+  }else if(totalTyped <= totalLength - 2){
+
+    if(stack.length > 1 && stack[stack.length - 1] === stack[stack.length - 2]){
+      countWords++;
+    }
+
+    check(inputField);
+    result();
+    console.log("finished")
+  }else {
+    result();
   }
 });
+function result(){
+  stopTimmer();
+  inputField.blur();
+}
+function checkWords(inputChar, index){
+  if(inputChar === " " || index === totalLength){
+    stack.push(incorrectCount);
+    if(stack.length === 1 && stack[stack.length - 1] === 0){
+      countWords++;
+    }else if(stack.length > 1 && stack[stack.length - 1] === stack[stack.length - 2]){
+      countWords++;
+    }
+  }
+}
+
+
+//TIMMER
+const timmer = document.querySelector(".time");
+let startTime = 0;
+let elapseTime = 0;
+let interval = 0;
+let isRunning = false;
+
+function updateTime(){
+
+  let time = elapseTime;
+  let hrs = Math.floor(time / 3600000);
+  let min = Math.floor((time % 3600000) / 60000);
+  let secs = Math.floor((time % 60000) / 1000);
+  timmer.textContent = `
+    ${min.toString().padStart(2, 0)}:${secs.toString().padStart(2, 0)}
+  `;
+
+}
+function startTimmer(){
+  if(isRunning) return;
+  isRunning = true;
+  startTime = Date.now() - elapseTime;
+  interval = setInterval(() =>{
+    elapseTime = Date.now() - startTime;
+    updateTime();
+  }, 1000);
+}
+function stopTimmer(){
+  clearInterval(interval);
+  isRunning = false;
+}
+
+//WPM
+const wpm = document.querySelector(".wpm");
+function wpmIs(){
+
+}
